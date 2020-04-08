99e933ba7534cbf4cc25156b63c3d7c6ca2c3220
Timur K <36789465+tlk13@users.noreply.github.com>
2020-04-08 23:27:34 +0200

    Random sampling of code snippets

--- visualisation/loadTrainData.py
+++ visualisation/loadTrainData.py
@@ -11,22 +11,22 @@ newPathIm = currentPathIm.replace("visualisation","data/trainImages.gz")
 currentPathLa = os.path.dirname(os.path.abspath(__file__))
 newPathLa = currentPathLa.replace("visualisation","data/trainLabels.gz")
 
-f = gzip.open(newPathIm, 'r')
-g = gzip.open(newPathLa, 'r')
+f = gzip.open(newPathIm, 'rb')
+g = gzip.open(newPathLa, 'rb')
 
 imageSize = 28
 numImages = 60000
-
+x_train = []; y_train = []
 
 
 buf = f.read(imageSize*imageSize*numImages)
 data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-data = data.reshape(numImages, imageSize, imageSize, 1)
+data = data.reshape(numImages, imageSize*imageSize)
 data /= data.max()
+x_train = data
 
 for i in range(numImages):
     buf = g.read(1)
     labels = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
-    print(labels)
-
-print(str(sys.getsizeof(data)))
+    if i<10: print(labels)
+    y_train.append(labels)
